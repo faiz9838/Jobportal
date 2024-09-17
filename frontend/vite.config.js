1555169d87c3f59cf746db72c2d057ff5bc49cc4
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-09-17 14:43:12 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import path from 'path';
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plubins: [react()],
-  root: "src",
+   plubins: [react()],
+  resolve: {
+     
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
